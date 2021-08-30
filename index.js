5539f2e843db44b499fa28803795fdf6aa5d6976
Ayele Lintamo <lashaaye@gmail.com>
2021-08-30 14:40:28 -0700

    second commit

--- index.js
+++ index.js
@@ -8,8 +8,30 @@ const questions = [
         type:'input',
         name:'github',
         message: 'What is your GitHub username?'
-    },
-]
+     },
+     {
+         tyep: 'input',
+         name: 'email',
+         message: 'what is your email address?'
+     },
+     {
+         tyep: 'input',
+         name: 'title',
+         message: 'what is your project name?'
+     },
+     {
+       tyep: 'input',
+       name: 'description',
+       message: 'please write a short description of your project'
+     },
+     {
+         tyep: 'list',
+         name: 'license',
+         message: 'what kind of license should your project have?',
+         choices:['MIT','APACHE 2.0','GPL 3.0', 'None']
+     },
+    
+]   
 
 function startApplication() {
     inquirer.prompt(questions)
